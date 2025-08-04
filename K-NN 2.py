eb3070b3a477bf49f18dde7cc96d0c66bbddc899
Thomas <https://github.com/THOMASBAIJU/Machine-Learning.git>
2025-08-04 06:13:36 +0530

    04-08-2025

--- K-NN 2.py
+++ K-NN 2.py
@@ -1,24 +1,39 @@
 import pandas as pd
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv("C:/Users/USER/Desktop/DataSet/Social_Network_Ads.csv")
 print(df.head())
-
 print("Missing Values")
 print(df.isnull().sum())
 
-x_features = df.drop(['User ID','Purchased'],axis=1)
-y_feather = df['Purchased']
+x_features = df.drop(['User ID','Purchased'], axis=1)
+y_feature = df['Purchased']
 
-X_train,X_test,y_train,y_test = train_test_split(x_features,y_feather, test_size=0.2, random_state= 42)
+print("x_features")
+print(x_features)
+print('y_features')
+print(y_feature)
 
-model = KNeighborsRegressor(n_neighbors=3)
-model.fit(X_train,y_train)
+x_features = pd.get_dummies(x_features, columns=['Gender'])
+x_features.info()
 
-y_predtrain=model.predict(X_train)
-y_predtest=model.predict(X_test)
 
-print("Accuracy test:",accuracy_score(y_test,y_predtrain))
-print("Accuracy train:",accuracy_score(y_test,y_predtrain))
+X_train, X_test, y_train, y_test = train_test_split(x_features, y_feature, test_size=0.2, random_state=42)
+
+print("Shape of feature (X) array:",x_features.shape)
+print("Shape of feature (Y) array:",y_feature.shape)
+
+model = KNeighborsClassifier(n_neighbors=5)
+model.fit(X_train, y_train)
+
+y_pred_train = model.predict(X_train)
+y_pred_test = model.predict(X_test)
+print("Accuracy train:", accuracy_score(y_train, y_pred_train))
+print("Accuracy test:", accuracy_score(y_test, y_pred_test))
+
+print("classification_report of Test:")
+print(classification_report(y_test,y_pred_test))
+print("classification_report of Train")
+print(classification_report(y_train,y_pred_train))
